7740ce41353e355f3b88d006131e3ceebb566b7a
Vaidehi Patel <vaidehipatel31101@gmail.com>
2024-03-22 10:46:59 +0530

    otp auth changes

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -98,12 +98,13 @@ function Login({ addUser }) {
                 alert('Please enter otp.')
             }
             else {
-                const data = phoneOtp.confirm(otp)
-                console.log(data);
-                if (phoneOtp.confirm(otp)) {
+                const userCredential = await phoneOtp.confirm(otp)
+                console.log(userCredential);
+                if (userCredential && userCredential.user) {
                     addUser(phoneNo, otp)
                     navigate('/dashboard');
                 }
+                else { console.error("Invalid User Credentials") }
             }
 
         } catch (error) {
